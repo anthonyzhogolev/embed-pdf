c21856647b049864fa9dd83e31fa577f8c8b84ae
Anthony <ale7496@gmail.com>
2021-08-05 17:13:10 +0500

    add placeholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import { useEffect, useState } from "react";
 import { axios } from "axios";
+import icon from "./onCountRounded.svg";
 
 import "./App.css";
+
+const saveDocument = ({ link, name = "", fileFormat = "pdf" }) => {
+  let fileName = name;
+  if (name && fileFormat) {
+    fileName = `${name}.${fileFormat}`;
+  }
+
+  const targetLink = document.createElement("a");
+  targetLink.download = fileName;
+  targetLink.href = link;
+  targetLink.click();
+  targetLink.remove();
+};
+
+const isSafari = () => {
+  const isAppleVendor = navigator.vendor.indexOf("Apple") > -1;
+  const isCriOS = navigator.userAgent.indexOf("CriOS") === -1;
+  const isFxiOS = navigator.userAgent.indexOf("FxiOS") === -1;
+  return isAppleVendor && isCriOS && isFxiOS;
+};
+
+const openDocument = (options) => {
+  if (isSafari()) {
+    saveDocument(options);
+  } else {
+    window.open(options.link, "_blank");
+  }
+};
 
 function usePdf() {
   const [link, setLink] = useState("");
@@ -32,11 +61,22 @@ function usePdf() {
       setLink("");
     };
   }, []);
-  return link;
+
+  return {
+    link: isSafari() ? null : link,
+    download: () => {
+      saveDocument({ link, name: "test-pdf", fileFormat: "pdf" });
+    },
+    preview: () => {
+      openDocument({ link, name: "test-pdf", fileFormat: "pdf" });
+    },
+  };
 }
 
 function App() {
-  const link = usePdf();
+  const { link, download, preview } = usePdf();
+
+  const showPreview = false && link !== null;
 
   const style = {
     width: "200px",
@@ -44,10 +84,17 @@ function App() {
   };
   return (
     <>
-      <object data={link} style={style}  type="application/pdf">
-        <span>NOT RENDERED...</span>
-      </object>
-      
+      <div onClick={preview} style={{ cursor: "pointer" }}>
+        {showPreview ? (
+          <object data={link} style={style} type="application/pdf">
+            <param name="data" value={link} />
+            <span>NOT RENDERED...</span>
+          </object>
+        ) : (
+          <img src={icon} className="icon" alt="logo" />
+        )}
+      </div>
+      <div onClick={download}>Download PDF</div>
     </>
   );
 }
