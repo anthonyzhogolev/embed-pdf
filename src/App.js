f261a79d7cf8045f7cddce82e3269940414969ca
Anthony <ale7496@gmail.com>
2021-08-05 11:21:13 +0500

    feat: add render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { axios } from "axios";
+
+import "./App.css";
+
+function usePdf() {
+  const [link, setLink] = useState("");
+  useEffect(() => {
+    console.log("useEffect...");
+
+    const fetchData = async () => {
+      const response = await fetch(
+        encodeURI(
+          "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf"
+        ),
+        {
+          // mode: "no-cors",
+        }
+      );
+
+      const buffer = await response.arrayBuffer();
+      console.log("buffer.length", buffer.length);
+      const myBlob = new Blob([buffer], { type: "application/pdf" });
+      const objectUrl = URL.createObjectURL(myBlob);
+      setLink(objectUrl);
+    };
+
+    fetchData();
+
+    return () => {
+      URL.revokeObjectURL(link);
+      setLink("");
+    };
+  }, []);
+  return link;
+}
 
 function App() {
+  const link = usePdf();
+
+  const style = {
+    width: "200px",
+    height: "200px",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <object data={link} style={style}  type="application/pdf">
+        <span>NOT RENDERED...</span>
+      </object>
+      
+    </>
   );
 }
 
